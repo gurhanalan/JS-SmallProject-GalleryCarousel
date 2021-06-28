1f42b394c89a4dd3111de34500a334f6d9947af1
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-28 07:31:21 +0300

    Add JS 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// SELECTIONS
 
+const btnLeft = document.querySelector(".gallery__btn--left");
+const btnRight = document.querySelector(".gallery__btn--right");
+
+const contents = document.querySelectorAll(".gallery__content__box");
+
+const dotsCont = document.querySelector(".gallery__dots");
+
+const dots = document.querySelectorAll(".gallery__dots__dot");
+
+let slideNum = 0;
+
+function slider(num) {
+    contents.forEach((box, index) => {
+        box.style.transform = `translateX(${100 * (index - num)}%)`;
+    });
+}
+slider(2);
