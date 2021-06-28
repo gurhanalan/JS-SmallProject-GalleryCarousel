f65c8725892429be70d923c6c0f0c0e510bea0d3
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-28 08:44:40 +0300

    Add JS dots functionality

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const contents = document.querySelectorAll(".gallery__content__box");
 
 const dotsCont = document.querySelector(".gallery__dots");
 
-const dots = document.querySelectorAll(".gallery__dots__dot");
-
 let slideNum = 0;
 
 // FUNCTIONS
@@ -17,21 +15,58 @@ function slider(num) {
         box.style.transform = `translateX(${100 * (index - num)}%)`;
     });
 }
-slider(0);
 
-// EVENT LISTENERS
+function createDots() {
+    contents.forEach((_, index) => {
+        dotsCont.insertAdjacentHTML(
+            "beforeend",
+            `<div class="gallery__dots__dot" data-order="${index}"></div>`
+        );
+    });
+}
 
-btnLeft.addEventListener("click", () => {
+function dotsHiglight() {
+    const dots = document.querySelectorAll(".gallery__dots__dot");
+
+    dots.forEach((dot) => {
+        dot.classList.remove("gallery__dots__dot--active");
+        if (dot.dataset.order == `${slideNum}`) {
+            dot.classList.add("gallery__dots__dot--active");
+        }
+    });
+}
+
+function slideRight() {
     slideNum++;
     if (slideNum >= contents.length) {
         slideNum = 0;
     }
     slider(slideNum);
-});
-btnRight.addEventListener("click", () => {
+    dotsHiglight();
+}
+function slideLeft() {
     slideNum--;
     if (slideNum < 0) {
         slideNum = contents.length - 1;
     }
     slider(slideNum);
+    dotsHiglight();
+}
+
+// EVENT LISTENERS
+
+slider(slideNum);
+createDots();
+dotsHiglight();
+
+btnRight.addEventListener("click", slideRight);
+
+btnLeft.addEventListener("click", slideLeft);
+
+dotsCont.addEventListener("click", (e) => {
+    if (e.target.classList.contains("gallery__dots__dot")) {
+        slideNum = +e.target.dataset.order;
+        slider(slideNum);
+        dotsHiglight();
+    }
 });
