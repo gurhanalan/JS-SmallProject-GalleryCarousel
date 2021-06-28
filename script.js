380233e78340fe003efc848996a7e901d18c975e
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-28 07:39:54 +0300

    Add JS Add arrow functionality

--- script.js
+++ script.js
@@ -11,9 +11,27 @@ const dots = document.querySelectorAll(".gallery__dots__dot");
 
 let slideNum = 0;
 
+// FUNCTIONS
 function slider(num) {
     contents.forEach((box, index) => {
         box.style.transform = `translateX(${100 * (index - num)}%)`;
     });
 }
-slider(2);
+slider(0);
+
+// EVENT LISTENERS
+
+btnLeft.addEventListener("click", () => {
+    slideNum++;
+    if (slideNum >= contents.length) {
+        slideNum = 0;
+    }
+    slider(slideNum);
+});
+btnRight.addEventListener("click", () => {
+    slideNum--;
+    if (slideNum < 0) {
+        slideNum = contents.length - 1;
+    }
+    slider(slideNum);
+});
